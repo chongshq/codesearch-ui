504a3ceffc0681661ae50391c441761453ed5e2e
Bennett Qian <qian.cs@outlook.com>
2017-05-16 14:16:03 +0800

    no message

--- dist/js/app/config.js
+++ dist/js/app/config.js
@@ -2,7 +2,7 @@
   var app = angular.module('engine');
 
   /* Edit with path of TC-JSON-API */
-  app.api = "http://localhost:3000/";
+  app.api = "http://codeapi.qiancs.cn/";
 
   /* Edit with path of the SpellWork API e.g http://trinitycore.cloudapp.net/spells/ */
   app.spellsApi = "";
